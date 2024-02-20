848e700e94a49764c333f8ef206a10afcbd36545
Rzqiwildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-02-20 11:37:05 +0700

    Update MTitik.java

--- Prak minggu pertama/MTitik.java
+++ Prak minggu pertama/MTitik.java
@@ -1,11 +1,18 @@
 public class MTitik {
     public static void main(String[] args) {
-        // mendeklarasi objek t1,t2 dan t3 dari kelas titik
-        Titik t1 = new Titik(1, 2);
-        Titik t2 = new Titik(3, 4);
+        // mendeklarasi objek t1 dan t2 dari kelas titik
+        Titik t1 = new Titik();
+        t1.setAbsis(1.0);
+        t1.setOrdinat(2.0);
+
+        Titik t2 = new Titik();
+        t2.setAbsis(3.0);
+        t2.setOrdinat(4.0);
+        
+        //mendeklarasi objek t3 menggunakan overload 
         Titik t3 = new Titik(5, 6);
 
-        System.out.println("Jumlah objek titik: 3");
+        System.out.println("Jumlah objek titik : 3");
         System.out.println("t1(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
         System.out.println("t2(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
         System.out.println("t3(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
