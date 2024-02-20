958ffccb8a108299a5dbe955130a003ea6ffea3d
Rzqiwildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-02-20 11:28:50 +0700

    Update MTitik.java

--- Prak minggu pertama/MTitik.java
+++ Prak minggu pertama/MTitik.java
@@ -13,7 +13,7 @@ public class MTitik {
         t3.setAbsis(5.0);
         t3.setOrdinat(6.0);
 
-        System.out.println("Jumlah objek titik: + t1.getCounterTitik()");
+        System.out.println("Jumlah objek titik: 3");
         System.out.println("t1(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
         System.out.println("t2(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
         System.out.println("t3(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
