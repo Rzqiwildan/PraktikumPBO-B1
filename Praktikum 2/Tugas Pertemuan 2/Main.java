b1766bd3cbc2444710c6e0edea932fa309d4fdee
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-03-11 17:51:53 +0700

    Add files via upload

---
+++ Praktikum 2/Tugas Pertemuan 2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount bank = new BankAccount();
+        System.out.println("Saldo sebelum transaksi: " + bank.getBalance());
+        System.out.println("Deposit sejumlah " + bank.deposit(500) + " Berhasil. Saldo saat ini " + bank.getBalance());
+        System.out.println("Penarikan sejumlah " + bank.withdraw(200) + " Berhasil. Saldo saat ini " + bank.getBalance());
+        System.err.println("Saldo setelah transaksi: " + bank.getBalance());
+    }
+}
