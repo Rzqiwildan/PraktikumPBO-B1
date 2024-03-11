794ebca5bb3cc86306ce443cfe36e750c9cc0775
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-03-11 17:54:54 +0700

    Update Main.java

--- Praktikum 2/Tugas Pertemuan 2/Main.java
+++ Praktikum 2/Tugas Pertemuan 2/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Saldo sebelum transaksi: " + bank.getBalance());
         System.out.println("Deposit sejumlah " + bank.deposit(500) + " Berhasil. Saldo saat ini " + bank.getBalance());
         System.out.println("Penarikan sejumlah " + bank.withdraw(200) + " Berhasil. Saldo saat ini " + bank.getBalance());
-        System.err.println("Saldo setelah transaksi: " + bank.getBalance());
+        System.err.println("Saldo setelah anda melakukan transaksi: " + bank.getBalance());
     }
 }
