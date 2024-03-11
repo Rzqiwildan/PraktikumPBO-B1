e6a67258b4c5cbafd5d033bcd01e5a719d6bce2d
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-03-11 17:56:49 +0700

    Update Main.java

--- Praktikum 2/Tugas Pertemuan 2/Main.java
+++ Praktikum 2/Tugas Pertemuan 2/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount bank = new BankAccount();
-        System.out.println("Saldo sebelum transaksi: " + bank.getBalance());
-        System.out.println("Deposit sejumlah " + bank.deposit(500) + " Berhasil. Saldo saat ini " + bank.getBalance());
-        System.out.println("Penarikan sejumlah " + bank.withdraw(200) + " Berhasil. Saldo saat ini " + bank.getBalance());
-        System.err.println("Saldo setelah anda melakukan transaksi: " + bank.getBalance());
+        BankAccount akunBank = new BankAccount();
+        System.out.println("Saldo sebelum transaksi: " + akunBank.getBalance());
+        System.out.println("Deposit sejumlah " + akunBank.deposit(500) + " Berhasil. Saldo saat ini " + akunBank.getBalance());
+        System.out.println("Penarikan sejumlah " + akunBank.withdraw(200) + " Berhasil. Saldo saat ini " + akunBank.getBalance());
+        System.err.println("Saldo setelah anda melakukan transaksi: " + akunBank.getBalance());
     }
 }
