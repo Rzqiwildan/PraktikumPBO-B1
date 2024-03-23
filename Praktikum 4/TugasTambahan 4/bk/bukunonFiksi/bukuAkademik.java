3719da487ae35ff7481155d4ffe4858b4ed6998a
wildan <gilangrizqi55@gmail.com>
2024-03-23 18:07:00 +0700

    Upload Tugas tambahan 4

---
+++ Praktikum 4/TugasTambahan 4/bk/bukunonFiksi/bukuAkademik.java
@@ -0,0 +1,10 @@
+package bukunonFiksi;
+
+import bukunonFiksi.bukuNonFiksi.*;
+
+public class bukuAkademik extends bukuNonFiksi {
+    public bukuAkademik(String judul, String penulis, String tahunTerbit, String genre, int jumlahHalaman,
+            double harga) {
+        super(judul, penulis, tahunTerbit, genre, jumlahHalaman, harga);
+    }
+}
