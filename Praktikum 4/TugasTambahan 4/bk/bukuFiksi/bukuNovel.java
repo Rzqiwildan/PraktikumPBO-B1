0169224b6316fec05a8d8960b498c4e21a3e3ba9
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2024-03-23 17:58:47 +0700

    Delete Praktikum 4/TugasTambahan 4/bk directory

--- Praktikum 4/TugasTambahan 4/bk/bukuFiksi/bukuNovel.java
+++
@@ -1,9 +0,0 @@
-package bukuFiksi;
-
-import bukuFiksi.bukuFiksi;
-
-public class bukuNovel extends bukuFiksi {
-    public bukuNovel(String judul, String penulis, String tahunTerbit, String genre, int jumlahHalaman, double harga) {
-        super(judul, penulis, tahunTerbit, genre, jumlahHalaman, harga);
-    }
-}
